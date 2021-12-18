296d496159eab7d9555469b4a981c8900967bc31
lithiumflowers <DanHangBu@outlook.com>
2021-12-18 16:05:29 +0800

    Add files via upload

---
+++ calculators.js
@@ -0,0 +1,6 @@
+var btn_txt = document.getElementsByClassName("btn");
+var display = document.getElementsByClassName("txt")[0];
+
+for(let i = 0;i < btn_txt.length;i ++){
+    btn_txt[i].onclick = function(){}
+}
